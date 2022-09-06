ac38a7f719797054c679ab5adfd7aa0eb782dc5e
n-e-o-n-7 <44162454+n-e-o-n-7@users.noreply.github.com>
2022-09-06 14:10:33 +0800

    action test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,27 @@
 import * as React from "react"
+
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+}
+
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      <h1 style={headingStyles}>
+        Congratulations
+        <br />
+      </h1>
+    </main>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
