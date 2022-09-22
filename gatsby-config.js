0968a9d029315ad1c5aaecf778448481cd7da662
n-e-o-n-7 <44162454+n-e-o-n-7@users.noreply.github.com>
2022-09-22 10:40:14 +0800

    prismic init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,22 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `blog`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-image',
+    {
+      resolve: "gatsby-source-prismic",
+      options: {
+          repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+          customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
+          linkResolver: require('./src/utils/linkResolver.js').linkResolver
+      },
+    }
+  ]
 };
